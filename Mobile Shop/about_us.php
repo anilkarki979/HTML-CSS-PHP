7555dc9cf5cf265935fe587e694e137250e8d43f
anilkarki979 <50413962+anilkarki979@users.noreply.github.com>
2022-08-14 20:58:38 +0545

    Add files via upload

---
+++ Mobile Shop/about_us.php
@@ -0,0 +1,39 @@
+<?php include('includes/header.php');?>
+<?php include('nav.php');?>	
+<div class="container">
+  <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">About Us</h2>
+            <p class="blog-post-meta">Updated At: January 24, 2020 by <a href="#">Anil Karki</a></p>
+
+            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <hr>
+            <p>The group members are:</p>
+            <blockquote>
+              <ul>
+    <li>Anil Karki</li>
+    <li>Pratham Dahal</li>
+    <li>Saugat Shrestha</li>
+    <li>Srijan Dangol</li>
+  </ul>
+            </blockquote>
+            
+
+          <nav>
+            <ul class="pager">
+              <li><a href="#">Previous</a></li>
+              <li><a href="#">Next</a></li>
+            </ul>
+          </nav>
+
+        </div>
+        </div><!-- /.blog-sidebar -->
+
+      </div>
+</div>
+
+
+<?php include('includes/footer.php');?>
